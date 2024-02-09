bad6b2e6cb87f698e977ab1becb3d7616d3bf0a0
nbachand <42705584+nbachand@users.noreply.github.com>
2024-02-09 12:24:11 -0800

    Walls seem to be okay but with num prec issue

--- model/WallSimulation.py
+++ model/WallSimulation.py
@@ -13,29 +13,35 @@ class WallSimulation:
         self.h = WallSides(4, 4) #fabric convection coefficient
         self.alpha = 0.7 #fabric absorptivity
         self.processMaterialDict(self.material_df)
-        self.x = np.arange(0, self.th + self.delx, self.delx)
+        self.x = np.linspace(0, self.th, self.n + 2)
 
     def processMaterialDict(self, material_df):
         """
         Process pandas df of materials that make up wall
         """
-        for index, row in material_df.iterrows():
-            if np.isnan(row["Thermal_Resistance"]) == False:
-                row["Thickness"] = self.delx # just place one point in the air gap
-                row["Conductivity"] = row["Thickness"] / row["Thermal_Resistance"] # convert R value
-                row["Density"] = 1 # arbitratily small
-                row["Specific_Heat"] = 1.005 # specific heat of air
+
         material_df["depth"] = material_df["Thickness"].cumsum()
         self.th = np.sum(material_df["Thickness"])
-        self.n = 10
-        self.delx = self.th / (self.n + 1) # reset delx to evenly divide the thickness
+        self.n = 9
+        self.delx = self.th / (self.n + 1) # set delx to evenly divide the thickness
+        for index, row in material_df.iterrows():
+            if row["key"] == "Material:AirGap":
+                self.n += 1 # add node in air gap
+                self.th += self.delx # recompute thickness
+                material_df.loc[index, "Thickness"] = self.delx # just place one point in the air gap
+                material_df.loc[index, "Conductivity"] = material_df.loc[index, "Thickness"] / material_df.loc[index, "Thermal_Resistance"] # convert R value
+                material_df.loc[index, "Density"] = 12 # large enough for reasonable time step
+                material_df.loc[index, "Specific_Heat"] = 1005 # specific heat of air
+        material_df["depth"] = material_df["Thickness"].cumsum()
+        print(material_df)
 
         self.kfs = np.zeros(self.n) #= 2300 #density of fabric
         self.rhofs = np.zeros(self.n) #= 750 #specific heat capacity of fabric
         self.Cfs = np.zeros(self.n) #= 0.8 #thermal conductivity of fabric
 
+        depth = 0
         for i in range(self.n):
-            depth = (i+1) / (self.n+1) * self.th
+            depth += self.delx
             material = material_df[material_df["depth"] >= depth].iloc[0]
             self.kfs[i] = material["Conductivity"]
             self.rhofs[i] = material["Density"]
@@ -44,7 +50,8 @@ class WallSimulation:
 
     def initialize(self, delt, TfF, TfB):
         # Scaling factors
-        self.lambda_vals = (self.kfs * delt) / (self.rhofs * self.Cfs * self.delx**2)
+        self.lambda_vals = (delt / self.delx**2) * self.kfs / (self.rhofs * self.Cfs)
+        print(f"maximum time step: {np.min(delt/self.lambda_vals)}")
         self.lambda_bound = WallSides()
         self.lambda_bound.front = self.kfs[0] / (self.h.front * self.delx)
         self.lambda_bound.back = self.kfs[-1] / (self.h.back * self.delx)
@@ -53,16 +60,34 @@ class WallSimulation:
         self.n = round(self.th / self.delx) - 1
         A_matrix = np.zeros((self.n, self.n))
         for i in range(self.n):
-            A_matrix[(0 + i)%self.n, i] = 1 - 2 * self.lambda_vals[i]
-            A_matrix[(1 + i)%self.n, i] = self.lambda_vals[0]
-            A_matrix[(-1 + i)%self.n, i] = self.lambda_vals[0]
+            A_matrix[i, i] = 1 - 2 * self.lambda_vals[i]
+            if i < self.n - 1:
+                A_matrix[i, i + 1] = self.lambda_vals[i]
+            if i > 0:
+                A_matrix[i, i - 1] = self.lambda_vals[i]
 
         A_matrix[0, 0] += self.lambda_vals[0] * self.lambda_bound.front / (1 + self.lambda_bound.front)
         A_matrix[-1, -1] += self.lambda_vals[-1] * self.lambda_bound.back / (1 + self.lambda_bound.back)
-        A_matrix[-1, 0] = 0
-        A_matrix[0, -1] = 0
 
         self.A = A_matrix
+
+        ###########################
+        # Plot the color plot
+        import plotly.graph_objs as go
+        import plotly.express as px
+        # Create a heatmap figure
+        fig = go.Figure(data=go.Heatmap(z=self.A, colorscale='Viridis'))
+
+        # Update layout
+        fig.update_layout(
+            title='Heatmap of Matrix',
+            xaxis_title='X-axis',
+            yaxis_title='Y-axis'
+        )
+
+        # Show the figure
+        fig.show()
+        ###########################
 
         self.b = np.zeros(self.n)
         self.T_prof = np.linspace(TfF, TfB, self.n + 2) #create a uniform temperature profile between Tff and Tfb of length n
@@ -80,10 +105,11 @@ class WallSimulation:
         self.T_prof = self.getWallProfile(TintRadF, TintRadB)
 
         Ef = WallSides()
-        # Ef.front = self.Af * (self.T_prof[1] - self.T_prof[0]) / self.delx
-        # Ef.back = self.Af * (self.T_prof[-2] - self.T_prof[-1]) / self.delx
+        Ef.front = self.Af * (self.T_prof[1] - self.T_prof[0]) / self.delx
+        Ef.back = self.Af * (self.T_prof[-2] - self.T_prof[-1]) / self.delx
         Ef.front = self.Af * (self.T_prof[0] - TintF) * self.h.front
         Ef.back = self.Af * (self.T_prof[-1] - TintB) * self.h.back
+        print(Ef.front, Ef.back)
         return Ef
 
     def getWallProfile(self, TintF, TintB):
@@ -94,4 +120,7 @@ class WallSimulation:
         return T_prof
 
     def get_Tf(self, T1, Tint, lambda_bound):
-        return (lambda_bound * T1 + Tint) / (1 + lambda_bound)
+        Tf = (lambda_bound * T1 + Tint) / (1 + lambda_bound)
+        if np.isnan(Tf) == True:
+            raise ValueError('Tf is nan')
+        return Tf
