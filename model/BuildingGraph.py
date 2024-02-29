b30a00aacb39c664cc61e4726c7f7c4f7627c3a3
nbachand <42705584+nbachand@users.noreply.github.com>
2024-02-29 11:07:40 -0800

    displaying room radiation graphs

--- model/BuildingGraph.py
+++ model/BuildingGraph.py
@@ -4,6 +4,36 @@ import networkx as nx
 from copy import deepcopy
 from model.utils import *
 
+def draw(G, weight = "weight"):
+        plt.figure()
+        # nx.draw(G, with_labels=True)
+
+        elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d[weight] > 0.5]
+        esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d[weight] <= 0.5]
+
+        pos = nx.spiral_layout(G)  # positions for all nodes - seed for reproducibility
+
+        # nodes
+        nx.draw_networkx_nodes(G, pos, node_size=200)
+
+        # edges
+        nx.draw_networkx_edges(G, pos, edgelist=elarge, width=2)
+        nx.draw_networkx_edges(
+            G, pos, edgelist=esmall, width=3, alpha=0.5, edge_color="b", style="dashed"
+        )
+
+        # node labels
+        nx.draw_networkx_labels(G, pos, font_size=10, font_family="sans-serif")
+        # edge weight labels
+        edge_labels = nx.get_edge_attributes(G, weight)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels, font_size=6)
+
+        ax = plt.gca()
+        ax.margins(0.08)
+        plt.axis("off")
+        plt.tight_layout()
+        plt.show()
+
 class BuildingGraph:
     def __init__(self, connectivityMatrix:np.array =  np.array([[]]), roomList:list = []):
         roomList = [(r, {}) if isinstance(r, str) else r for r in roomList] # adding empty dicts if no dict is provided
@@ -25,33 +55,7 @@ class BuildingGraph:
                         )
 
     def draw(self):
-        plt.figure()
-        # nx.draw(self.G, with_labels=True)
-
-        elarge = [(u, v) for (u, v, d) in self.G.edges(data=True) if d["weight"] > 0.5]
-        esmall = [(u, v) for (u, v, d) in self.G.edges(data=True) if d["weight"] <= 0.5]
-
-        pos = nx.spiral_layout(self.G)  # positions for all nodes - seed for reproducibility
-
-        # nodes
-        nx.draw_networkx_nodes(self.G, pos, node_size=200)
-
-        # edges
-        nx.draw_networkx_edges(self.G, pos, edgelist=elarge, width=2)
-        nx.draw_networkx_edges(
-            self.G, pos, edgelist=esmall, width=3, alpha=0.5, edge_color="b", style="dashed"
-        )
-
-        # node labels
-        nx.draw_networkx_labels(self.G, pos, font_size=10, font_family="sans-serif")
-        # edge weight labels
-        edge_labels = nx.get_edge_attributes(self.G, "weight")
-        nx.draw_networkx_edge_labels(self.G, pos, edge_labels, font_size=6)
-
-        ax = plt.gca()
-        ax.margins(0.08)
-        plt.axis("off")
-        plt.tight_layout()
+        draw(self.G)
 
     def updateEdges(self, properties: dict, nodes = None, edges = None):
         """
