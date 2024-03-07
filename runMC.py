7363d8d61613fa97744594a27f98c30891ebcce5
nbachand <42705584+nbachand@users.noreply.github.com>
2024-03-07 14:31:08 -0800

    added serial and generalized

--- runMC.py
+++ runMC.py
@@ -3,13 +3,23 @@ import random
 from myBuilding import runMyBEM
 
 
-def runMC(weatherData, materials):
-    client = Client()
-    print(client)
+def runMC(inputs: list, parallel = True):
+    if parallel:
+        try:
+            client.shutdown()
+        except:
+            pass
+        client = Client()
+        display(client)
 
-    realizations = client.map(runMyBEM, weatherData, materials, makePlots=False, verbose = False)
+        realizations = client.map(runMyBEM, *inputs, makePlots=False, verbose = False)
 
-    realizationOutputs = client.gather(realizations)
+        realizationOutputs = client.gather(realizations)
 
-    client.shutdown()
+        client.shutdown()
+    else:
+        for i in range(len(inputs[0])):
+            serial_inputs = [input[i] for input in inputs]
+            realizationOutputs = runMyBEM(*serial_inputs, makePlots=False, verbose = False)
+    
     return realizationOutputs
